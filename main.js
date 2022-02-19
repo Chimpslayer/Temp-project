887ce178769ff6e1840e7740c63c65cff940ddc5
Chimpslayer <94501127+Chimpslayer@users.noreply.github.com>
2022-02-18 20:55:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 // The value of kevin is constant
-const kelvin = 293
+const kelvin = 293;
 
 // Convert kelvin to celsius by subtracting 273
-var celsius = kelvin - 273
+var celsius = kelvin - 273;
 
 // This is the formula for newton rounded down.
-var newton = Math.floor(celsius * (33/100))
+var newton = Math.floor(celsius * (33/100));
 
 // This is the formula for fahrenheit rounded down
-var fahrenheit = Math.floor(celsius * (9/5) + 32)
+var fahrenheit = Math.floor(celsius * (9/5) + 32);
 
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
 // 
